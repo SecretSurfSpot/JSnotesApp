719fe71ce92676df3bd35efa7ab5af2ae41bde06
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-09-21 14:43:58 +0100

    Refactor note-list-view-test limit 20 chars)

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,18 +1,13 @@
 var notelist = new NoteList()
-var note_1 = "Hello my name is Freddie"
-var note_2 = "Hello my name is Andrew"
-var note_1_substr = note_1.substr(0, 20)
-var note_2_substr = note_2.substr(0, 20)
 
-
-notelist.addNote(note_1_substr)
-notelist.addNote(note_2_substr)
+notelist.addNote("Hello my name is Freddie")
+notelist.addNote("Hello my name is Andrew")
 var listView = new NoteListView(notelist)
 
-var expectedOutputText = "<ul><li><div>" + note_1_substr + "</div></li><li><div>" + note_2_substr + "</div></li></ul>"
+var expectedOutputText = "<ul><li><div>Hello my name is Fre</div></li><li><div>Hello my name is And</div></li></ul>"
 
 function testDisplayNotes() {
-  assert.isTrue("NoteListView.displayNotes", listView.displayNotes() === expectedOutputText)
+  assert.isTrue("NoteListView.displayNotes limits output to 20 chars", listView.displayNotes() === expectedOutputText)
 }
 
 testDisplayNotes()
